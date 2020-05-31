9971a80b66c7c5a98ac372278e23708ea82e29ae
zac <a19860101@gmail.com>
2020-05-31 13:42:17 +0800

    20200531

--- basic/function.php
+++ basic/function.php
@@ -9,12 +9,24 @@
     // echo test();
     // $q = test();
     // echo $q;
-    function q($x,$y){
-        return $x + $y;
-    }
-    function qq($x=100,$y=1000){
-        return $x + $y;
-    }
+    // function q($x,$y){
+        // return $x + $y;
+    // }
+    // function qq($x=100,$y=1000){
+        // return $x + $y;
+    // }
     // echo q(123);
-    echo qq(,321);
+    // echo qq(,321);
+    $user = "John";
+    function q(){
+        global $user;
+        return $user;
+    }
+    function q2(){
+        global $user;
+        echo $user;
+    }
+    echo q();
+
+    q2();
 
---
+++ basic/include_require.php
@@ -0,0 +1,22 @@
+<?php
+    // include("na.php");
+    // require("na.php");
+
+    // include("nav.php");
+    // include("nav.php");
+    // include("nav.php");
+    // require("nav.php");
+    // require("nav.php");
+    // require("nav.php");
+    // include_once("nav.php");
+    // include_once("nav.php");
+    // include_once("nav.php");
+    // include_once("nav.php");
+    // include_once("nav.php");
+    require_once("nav.php");
+    require_once("nav.php");
+    require_once("nav.php");
+    require_once("nav.php");
+    require_once("nav.php");
+    require_once("nav.php");
+    // echo "HELLO";
