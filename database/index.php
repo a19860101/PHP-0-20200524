f58cdf92a1ce253f0af099b7e9d41c94ad6b7512
Zac <a19860101@gmail.com>
2020-06-07 15:38:48 +0800

    20200607

--- database/index.php
+++ database/index.php
@@ -4,6 +4,7 @@
     $result = mysqli_query($conn,$sql);
 ?>
 <?php include("template/header.php");?>
+<?php include("template/nav.php");?>
 <?php
     // echo "<ul>";
     // while($row=mysqli_fetch_assoc($result)){
@@ -13,16 +14,23 @@
     // }
     // echo "</ul>";
 ?>
-<table>
-<?php while($row=mysqli_fetch_assoc($result)){?> 
-    <tr>
-        <td><?php echo $row["id"];?></td>
-        <td><?php echo $row["name"];?></td>
-        <td><?php echo $row["phone"];?></td>
-        <td><?php echo $row["mail"];?></td>
-        <td><?php echo $row["gender"];?></td>
-        <td><?php echo $row["create_at"];?></td>
-    </tr>
-<?php } ?>
-</table>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-10">
+            <table class="table">
+            <?php while($row=mysqli_fetch_assoc($result)){?> 
+                <tr>
+                    <td><?php echo $row["id"];?></td>
+                    <td><?php echo $row["name"];?></td>
+                    <td><?php echo $row["phone"];?></td>
+                    <td><?php echo $row["mail"];?></td>
+                    <td><?php echo $row["gender"];?></td>
+                    <td><?php echo $row["create_at"];?></td>
+                </tr>
+            <?php } ?>
+            </table>
+        </div>
+    </div>
+</div>
+
 <?php include("template/footer.php");?>
