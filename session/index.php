0e78298495b352cc8c54c6b1c2d327b95df5fb22
Zac <a19860101@gmail.com>
2020-06-14 15:08:28 +0800

    20200614

--- session/index.php
+++ session/index.php
@@ -1,9 +1,24 @@
-<?php
-    session_start();
-    echo $_SESSION["USER"];
-    
-
-
-
-?>
-<a href="destroy.php?q=true">delete session</a>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="login.php" method="post">
+         <input type="text" name="user">
+         <input type="pw" name="pw">
+         <input type="submit" value="登入">
+    </form>
+    <a href="logout.php?q=true">登出</a>
+    <?php
+        session_start();
+        if($_SESSION){
+            echo $_SESSION["USER"]."你好";
+        }else{
+            echo "請登入";
+        }
+    ?>
+</body>
+</html>
---
+++ session/login.php
@@ -0,0 +1,5 @@
+<?php
+    session_start();
+    $_SESSION["USER"] = $_POST["user"];
+
+    header("location:index.php");
