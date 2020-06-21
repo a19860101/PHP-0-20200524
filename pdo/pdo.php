1bc146eee0bb4fb696c959e2e3cf7eae3ccc107e
Zac <a19860101@gmail.com>
2020-06-21 11:27:59 +0800

    20200621

---
+++ pdo/pdo.php
@@ -0,0 +1,9 @@
+<?php
+    $db_host = "localhost";
+    $db_user = "admin";
+    $db_pw = "admin";
+    $db_name = "gjun";
+    $db_charset="utf8mb4";
+
+    $dsn = "mysql:host={$db_host};dbname={$db_name};charset={$db_charset}";
+    $pdo = new PDO($dsn,$db_user,$db_pw);
