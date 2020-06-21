becf90f191df91188374979eaab84dfaf1024046
Zac <a19860101@gmail.com>
2020-06-21 14:28:51 +0800

    20200621

--- pdo/detail.php
+++ pdo/detail.php
@@ -6,12 +6,20 @@
         $stmt = $pdo->prepare($sql);
         $stmt->execute([$id]);
         $row = $stmt->fetch();
-        echo $row["name"];
     }catch(PDOException $e){
         echo $e->getMessage();
     }
 ?>
 <?php include("template/header.php");?>
 <?php include("template/nav.php");?>
-
+<div class="container">
+    <?php 
+        echo $row["name"];
+        echo $row["mail"];
+        echo $row["phone"];
+        echo $row["gender"];
+        echo $row["create_at"];
+    
+    ?>
+</div>
 <?php include("template/footer.php");?>
