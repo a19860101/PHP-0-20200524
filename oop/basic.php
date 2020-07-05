d094a902c268cefe76fa3c3e50f60e2f9f86dc7d
Zac <a19860101@gmail.com>
2020-07-05 10:12:16 +0800

    20200705

--- oop/basic.php
+++ oop/basic.php
@@ -3,7 +3,12 @@
         public $a=100;
         // protected $b;
         // private $c;
+
+        static function go(){
+            return 999;
+        }
     }
     //建立實體
     $user = new User;
-    echo $user->a;
+    // echo $user->a;
+    echo $user->go();
