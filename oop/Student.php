db06912b5394e4127005cce00283a3b292536bad
Zac <a19860101@gmail.com>
2020-07-05 12:04:05 +0800

    20200705

--- oop/Student.php
+++ oop/Student.php
@@ -1,4 +1,17 @@
 <?php
     class Student extends DB {
-
+        function showAll(){
+            try {
+                $sql = "SELECT * FROM students";
+                $stmt = $this->connect()->prepare($sql);
+                $stmt->execute();
+                $rows = array();
+                while($row = $stmt->fetch()){
+                    $rows[] = $row;
+                }
+                return $rows;
+            }catch(PDOException $e){
+                echo $e->getMessage();
+            }
+        }
     }
--- oop/index.php
+++ oop/index.php
@@ -1,6 +1,8 @@
 <?php
     include("pdo.php");
     include("Student.php");
+    $students = new Student;
+    $rows = $students->showAll();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -10,6 +12,35 @@
     <title>Document</title>
 </head>
 <body>
-    
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-10">
+            <table class="table">
+                <tr>
+                    <th>#</th>
+                    <th>姓名</th>
+                    <th>建立時間</th>
+                    <th>動作</th>
+                </tr>
+                <?php foreach($rows as $row){ ?>
+                <tr>
+                    <td><?php echo $row["id"];?></td>
+                    <td><?php echo $row["name"];?></td>
+                    <td><?php echo $row["create_at"];?></td>
+                    <td>
+                        <form action="delete.php" method="post">
+                            <input type="hidden" name="id" value="<?php echo $row["id"];?>">
+                            <input type="submit" value="刪除" class="btn btn-danger btn-sm" onclick="return confirm('確認刪除?')">
+                        </form>
+                        <!-- <a href="delete.php?id=<?php #echo $row["id"];?>">刪除</a> -->
+                        <a href="edit.php?id=<?php echo $row["id"];?>" class="btn btn-success btn-sm">編輯</a>
+                        <a href="detail.php?id=<?php echo $row["id"];?>" class="btn btn-primary  btn-sm">檢視</a>
+                    </td>
+                </tr>
+                <?php }?>
+            </table>
+        </div>
+    </div>
+</div>
 </body>
 </html>
