4db6377dfe545f0de35545f394e1536324ca3a20
Zac <a19860101@gmail.com>
2020-07-05 10:47:30 +0800

    20200705

--- oop/basic.php
+++ oop/basic.php
@@ -1,14 +1,29 @@
 <?php
-    class User {
+    class Phone {
         public $a=100;
+        public $b = 60;
         // protected $b;
-        // private $c;
+        private $c = 999;
 
-        static function go(){
-            return 999;
+        function go(){
+           return $this->a + $this->b;
+        }
+        function gogogo(){
+            return $this->c;
+        }
+        static function move(){
+            $x = "hello";
+            return $x;
         }
     }
     //建立實體
-    $user = new User;
-    // echo $user->a;
-    echo $user->go();
+    echo  Phone::move();
+    // $iphone = new Phone;
+    // echo $iphone->gogogo();
+    // echo $iphone->move();
+    // echo $iphone->go();
+    // echo $iphone->b;
+    // echo $iphone->c;
+    // $iphone -> a = "black"; 
+    // $samsung = new Phone;
+    // $samsung -> a = "red";
